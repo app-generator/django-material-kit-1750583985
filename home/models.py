8d30f8dfb09baa2f20c3ac9cae0500b592cb6f7a
App Generator <51070104+app-generator@users.noreply.github.com>
2025-06-22 12:19:53 +0300

    initial commit

---
+++ home/models.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Appointment(models.Model):
+
+    #__Appointment_FIELDS__
+    name = models.TextField(max_length=255, null=True, blank=True)
+    company = models.TextField(max_length=255, null=True, blank=True)
+    workshop = models.TextField(max_length=255, null=True, blank=True)
+    question = models.TextField(max_length=255, null=True, blank=True)
+    createdat = models.DateTimeField(blank=True, null=True, default=timezone.now)
+
+    #__Appointment_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Appointment")
+        verbose_name_plural = _("Appointment")
+
+
+
+#__MODELS__END
